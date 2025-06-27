889f3f1632dbb6b6959a469996b7ab4cb053df76
Miramaged802 <miramaged345@gmail.com>
2025-06-27 08:03:39 +0300

    edit responsive

--- src/pages/seller/components/SellerHome.jsx
+++ src/pages/seller/components/SellerHome.jsx
@@ -203,7 +203,7 @@ const SellerHome = () => {
       </div>
 
       {/* Stats Cards */}
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 md:gap-6">
         {stats.map((stat, index) => (
           <StatsCard key={index} {...stat} />
         ))}
